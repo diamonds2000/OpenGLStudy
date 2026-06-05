feac3a5be086ae4dfbbe4dc12b1eb5f2a954e70a
diamonds2000 <diamonds2000@163.com>
2026-06-05 15:21:17 +0800

    Init

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+#include "render.h"
+
+int main()
+{
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render OpenGL here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
---
+++ render.h
@@ -0,0 +1,6 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include <GLFW/glfw3.h>
+
+#endif // RENDER_H
